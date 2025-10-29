de9320001d1efd46aaba839bf0d5e75060dc4341
ewilly <ewilly@free.fr>
2025-10-29 22:09:27 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,17 +6,20 @@
 
 # Set permissions
 myynh_set_permissions () {
-	chown -R "$app":"$app" "$install_dir"
-	find "$install_dir" -type f -exec chmod 750 {} ';'
-	find "$install_dir" -type d -exec chmod 755 {} ';'
+	chown -R "$app": "$install_dir"
+	chmod u=rwx,g=rwx,o= "$install_dir"
+	find "$install_dir" -type f -exec chmod u=rwx,g=rwx,o= {} ';'
+	find "$install_dir" -type d -exec chmod u=rwx,g=rwx,o= {} ';'
 
 	mkdir -p "$data_dir/plugins"
-	chown -R "$app":"$app" "$data_dir"
-	find "$data_dir" -type f -exec chmod 750 {} ';'
-	find "$data_dir" -type d -exec chmod 755 {} ';'
+	chown -R "$app": "$data_dir"
+	chmod u=rwx,g=rwx,o= "$data_dir"
+	find "$data_dir" -type f -exec chmod u=rwx,g=rwx,o= {} ';'
+	find "$data_dir" -type d -exec chmod u=rwx,g=rwx,o= {} ';'
 
 	mkdir -p "/var/log/$app"
-	chown -R "$app":"$app" "/var/log/$app"
-	find "/var/log/$app" -type f -exec chmod 640 {} ';'
-	find "/var/log/$app" -type d -exec chmod 755 {} ';'
+	chown -R "$app": "/var/log/$app"
+	chmod u=rw,g=rw,o= "/var/log/$app"
+	find "/var/log/$app" -type f -exec chmod u=rw,g=rw,o= {} ';'
+	find "/var/log/$app" -type d -exec chmod u=rw,g=rw,o= {} ';'
 }
