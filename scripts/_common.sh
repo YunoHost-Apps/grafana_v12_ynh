18a032f6141818f7cc97c23b9aecb7de51563dc6
ewilly <ewilly@free.fr>
2025-10-30 07:09:28 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,22 +4,18 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
+log_file="/var/log/$app/${app%_*}.log"
+
 # Set permissions
 myynh_set_permissions () {
 	chown -R "$app": "$install_dir"
-	chmod u=rwx,g=rwx,o= "$install_dir"
-	find "$install_dir" -type f -exec chmod u=rwx,g=rwx,o= {} ';'
-	find "$install_dir" -type d -exec chmod u=rwx,g=rwx,o= {} ';'
+	chmod -R u=rwx,g=rwx,o= "$install_dir"
 
 	mkdir -p "$data_dir/plugins"
 	chown -R "$app": "$data_dir"
-	chmod u=rwx,g=rwx,o= "$data_dir"
-	find "$data_dir" -type f -exec chmod u=rwx,g=rwx,o= {} ';'
-	find "$data_dir" -type d -exec chmod u=rwx,g=rwx,o= {} ';'
+	chmod -R u=rwx,g=rwx,o= "$data_dir"
 
 	mkdir -p "/var/log/$app"
 	chown -R "$app": "/var/log/$app"
-	chmod u=rw,g=rw,o= "/var/log/$app"
-	find "/var/log/$app" -type f -exec chmod u=rw,g=rw,o= {} ';'
-	find "/var/log/$app" -type d -exec chmod u=rw,g=rw,o= {} ';'
+	chmod u=rwx,g=rx,o=rw "/var/log/$app"
 }
